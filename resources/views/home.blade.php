43855e6b687fbd7e862e29dfa66264f8451df7af
Alfeo Raymond <ngelime07@gmail.com>
2020-10-27 08:45:59 +0300

    homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,55 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+    <div class="row">
+        <div class="col-md-4">
+            <div class="row">
+                <div class="col">
+                    <div class="card bg-primary border-primary text-light">
+                        <div class="card-body">
+                            <h5 class="card-title"> <b> <i class="fa fa-inbox" aria-hidden="true"></i> </b> Inbox</h5>
+                            <p class="card-text">Content</p>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col">
+                    <div class="card bg-success border-success text-light">
+                        <div class="card-body">
+                            <h5 class="card-title"> <b> <i class="fa fa-user-plus" aria-hidden="true"></i> </b> Subscribers </h5>
+                            <p class="card-text">Content</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col">
+                    <div class="card bg-danger border-danger text-light">
+                        <div class="card-body">
+                            <h5 class="card-title"><b> <i class="fa  fa-external-link"></i> </b> Forwards </h5>
+                            <p class="card-text">Content</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br>
+        </div>
+        <div class="col">
+            <div class="row">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Title</h5>
+                            <p class="card-text">Content</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+    <br>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans&display=swap" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -23,8 +23,8 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand" href="{{ url('/home') }}">
+                  <b>{{ config('app.name', 'Laravel') }}</b>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -32,8 +32,12 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
+                    <ul class="navbar-nav m-auto">
+                        <li class="nav-item">
+                            <a href="" class="nav-link">
+                                <i class="fa fa-home" aria-hidden="true"> </i> Home
+                            </a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -71,9 +75,10 @@
                 </div>
             </div>
         </nav>
-<div class="bg-primary" style="height: 2px;">
+        <div class="bg-primary" style="height: 3px;">
 
-</div>
+        </div>
+
         <main class="py-4">
             @yield('content')
         </main>
