57de53e3188fc8a6c9ee4c04a5231bcafd32a26d
Alfeo Raymond <ngelime07@gmail.com>
2021-07-20 20:44:50 +0300

    making login page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -54,8 +54,8 @@
         <h1>Share emails, information from one app!</h1>
         <p class="mg-b-25">When your dashboard or app is attractive to use, your users will not simply be using it, they’ll look forward to using it. This means that you should fashion the look and feel of your interface for your users.</p>
         <p>
-          <a id="azDemoBtn" href="#azDemo" class="btn btn-primary flex-1 mg-r-10">Login</a>
-          <a href="#" class="btn btn-primary flex-1">Register</a>
+          <a id="loginBtn" href="#login" class="btn btn-primary flex-1 mg-r-10">Login</a>
+          <a id="registerBtn" href="#register" class="btn btn-primary flex-1">Register</a>
         </p>
 
         <nav>
@@ -67,8 +67,99 @@
     </div><!-- az-home-slider -->
 
 
+    <div id="login" class="az-content az-content-choose-demo">
+        <div class="container">
+            {{--  --}}
+            <div class="az-signup-wrapper">
+                <div class="az-column-signup-left">
+                  <div>
+                    <i class="typcn typcn-chart-bar-outline"></i>
+                    <h1 class="az-logo">az<span>i</span>a</h1>
+                    <h5>Responsive Modern Dashboard &amp; Admin Template</h5>
+                    <p>We are excited to launch our new company and product Azia. After being featured in too many magazines to mention and having created an online stir, we know that BootstrapDash is going to be big. We also hope to win Startup Fictional Business of the Year this year.</p>
+                    <p>Browse our site and see for yourself why you need Azia.</p>
+                    <a href="index.html" class="btn btn-outline-indigo">Learn More</a>
+                  </div>
+                </div><!-- az-column-signup-left -->
+                <div class="az-column-signup">
+                    <h1 class="az-logo">az<span>i</span>a</h1>
+                    <div class="az-signup-header">
+                      <h2>Get Started</h2>
+                      <h4>It's free to signup and only takes a minute.</h4>
+
+                    <form action="https://www.bootstrapdash.com/demo/azia/v1.0.0/template/page-profile.html">
+                      <div class="form-group">
+                        <label>Email</label>
+                        <input type="text" class="form-control" placeholder="Enter your email">
+                      </div><!-- form-group -->
+                      <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" class="form-control" placeholder="Enter your password">
+                      </div><!-- form-group -->
+                      <button class="btn btn-az-primary btn-block">Create Account</button>
+                    </form>
+                  </div><!-- az-signup-header -->
+                  <div class="az-signup-footer">
+                    <p>Don't an account? <a href="#register">Register</a></p>
+                  </div><!-- az-signin-footer -->
+                </div><!-- az-column-signup -->
+              </div><!-- az-signup-wrapper -->
+        </div><!-- container-fluid -->
+      </div><!-- az-content -->
+
+      <div id="register" class="az-content az-content-choose-demo">
+        <div class="container">
+            {{--  --}}
+            <div class="az-signup-wrapper">
+                <div class="az-column-signup-left">
+                  <div>
+                    <i class="typcn typcn-chart-bar-outline"></i>
+                    <h1 class="az-logo">az<span>i</span>a</h1>
+                    <h5>Responsive Modern Dashboard &amp; Admin Template</h5>
+                    <p>We are excited to launch our new company and product Azia. After being featured in too many magazines to mention and having created an online stir, we know that BootstrapDash is going to be big. We also hope to win Startup Fictional Business of the Year this year.</p>
+                    <p>Browse our site and see for yourself why you need Azia.</p>
+                    <a href="index.html" class="btn btn-outline-indigo">Learn More</a>
+                  </div>
+                </div><!-- az-column-signup-left -->
+                <div class="az-column-signup">
+                  <div class="az-signup-header">
+                    <h2>Register</h2>
+
+                    <form action="https://www.bootstrapdash.com/demo/azia/v1.0.0/template/page-profile.html">
+                      <div class="form-group">
+                        <label>Firstname &amp; Lastname</label>
+                        <input type="text" class="form-control" placeholder="Enter your firstname and lastname">
+                      </div><!-- form-group -->
+                      <div class="form-group">
+                        <label>Email</label>
+                        <input type="text" class="form-control" placeholder="Enter your email">
+                      </div><!-- form-group -->
+                      <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" class="form-control" placeholder="Enter your password">
+                      </div><!-- form-group -->
+                      <button class="btn btn-az-primary btn-block">Create Account</button>
+
+                      <button class="btn btn-az-primary btn-block">Create Account</button>
+                      <button class="btn btn-az-primary btn-block">Create Account</button>
+                      <button class="btn btn-az-primary btn-block">Create Account</button>
+                      
+                        <div class="col-sm-4"><a href="#" class="btn btn-danger"><i class="fab fa-google"></i> Signup with Google</a></div>
+                        <div class="col-sm-4"><a href="#" class="btn btn-primary"><i class="fab fa-facebook-f"></i> Signup with Facebook</a></div>
+                        <div class="col-sm-4"><a href="#" class="btn btn-primary"><i class="fab fa-twitter"></i> Signup with Twitter</a></div>
+
+                    </form>
+                  </div><!-- az-signup-header -->
+                  <div class="az-signup-footer">
+                    <p>Already have an account? <a href="#login">Login</a></p>
+                  </div><!-- az-signin-footer -->
+                </div><!-- az-column-signup -->
+              </div><!-- az-signup-wrapper -->
+        </div><!-- container-fluid -->
+      </div><!-- az-content -->
+
         {{-- <div>&copy; {{date('Y')}} {{ config('app.name', 'Laravel') }}.</div> --}}
-       
+
     <script src="{{asset('rsc/lib/jquery/jquery.min.js')}}"></script>
     <script src="{{asset('rsc/lib/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
     <script src="{{asset('rsc/lib/ionicons/ionicons.js')}}"></script>
