5403d1eb37e3bf2827a69940c0e37c7082311310
Alfeo Raymond <ngelime07@gmail.com>
2021-07-22 20:46:17 +0300

    changed sidebar orientation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,15 +23,9 @@
 
     <div class="az-sidebar">
         <div class="az-sidebar-header">
-          <a href="index.html" class="az-logo">az<span>i</span>a</a>
+            <a href="{{ url('/') }}" class="az-logo"><img src="{{asset('images/arifu-01.png')}}" alt="{{ config('app.name', 'Laravel') }}" width="50%"></a>
         </div><!-- az-sidebar-header -->
-        <div class="az-sidebar-loggedin">
-          <div class="az-img-user online"><img src="{{asset('rsc/img/faces/face1.jpg')}}" alt=""></div>
-          <div class="media-body">
-            <h6>Aziana Pechon</h6>
-            <span>Premium Member</span>
-          </div><!-- media-body -->
-        </div><!-- az-sidebar-loggedin -->
+
         <div class="az-sidebar-body">
           <ul class="nav">
             <li class="nav-label">Main Menu</li>
